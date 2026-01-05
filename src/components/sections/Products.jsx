8db93938541a0cf6b57b224b93f4b358d5bf3cb2
Zidan-ob1 <zidanewibowo08@gmail.com>
2026-01-05 13:23:46 +0700

    update: pindah file foto

--- src/components/sections/Products.jsx
+++ src/components/sections/Products.jsx
@@ -8,78 +8,78 @@ export const Products = ({ isLoggedIn, onLoginClick }) => {
 
   const filters = ["All", "CPU", "Motherboard", "Graphic Card", "PC Case", "Memory / RAM", "Power Supply"];
 
-  const allProducts = [
-    {
-      id: 1,
-      name: "Intel Core i9-14900K",
-      price: 9500000,
-      tag: "CPU",
-      image: "src/assets/processor.png" 
-    },
-    {
-      id: 2,
-      name: "Nvidia GeForce RTX 4090",
-      price: 28000000,
-      tag: "Graphic Card",
-      image: "src/assets/vga.png" 
-    },
-    {
-      id: 3,
-      name: "ASUS ROG Maximus Z790",
-      price: 8500000,
-      tag: "Motherboard",
-      image: "src/assets/motherboard.png" 
-    },
-    {
-      id: 4,
-      name: "NZXT H9 Flow White",
-      price: 2600000,
-      tag: "PC Case",
-      image: "src/assets/casing.png" 
-    },
-    {
-      id: 5,
-      name: "Corsair Vengeance RGB 32GB DDR5",
-      price: 2100000,
-      tag: "Memory / RAM",
-      image: "src/assets/corsairVengeance.jpg"
-    },
-    {
-      id: 6,
-      name: "Corsair RM1000x 1000W Gold",
-      price: 2800000,
-      tag: "Power Supply",
-      image: "src/assets/CorsairSupply.jpg"
-    },
-    {
-      id: 7,
-      name: "AMD Ryzen 9 7950X",
-      price: 8900000,
-      tag: "CPU",
-      image: "src/assets/Ryzen9.jpg"
-    },
-    {
-      id: 8,
-      name: "Gigabyte GeForce RTX 4070",
-      price: 10500000,
-      tag: "Graphic Card",
-      image: "src/assets/rtx4070.png"
-    },
-    {
-      id: 9,
-      name: "MATX Mini ATX",
-      price: 500000,
-      tag: "PC Case",
-      image: "src/assets/casingATX.jpeg"
-    },
-     {
-      id: 10,
-      name: "Intel Core i5-13400f",
-      price: 3500000,
-      tag: "CPU",
-      image: "src/assets/intelCorei5.png"
-    }
-  ];
+ const allProducts = [
+  {
+    id: 1,
+    name: "Intel Core i9-14900K",
+    price: 9500000,
+    tag: "CPU",
+    image: "/processor.png" 
+  },
+  {
+    id: 2,
+    name: "Nvidia GeForce RTX 4090",
+    price: 28000000,
+    tag: "Graphic Card",
+    image: "/vga.png" 
+  },
+  {
+    id: 3,
+    name: "ASUS ROG Maximus Z790",
+    price: 8500000,
+    tag: "Motherboard",
+    image: "/motherboard.png" 
+  },
+  {
+    id: 4,
+    name: "NZXT H9 Flow White",
+    price: 2600000,
+    tag: "PC Case",
+    image: "/casing.png" 
+  },
+  {
+    id: 5,
+    name: "Corsair Vengeance RGB 32GB DDR5",
+    price: 2100000,
+    tag: "Memory / RAM",
+    image: "/corsairVengeance.jpg"
+  },
+  {
+    id: 6,
+    name: "Corsair RM1000x 1000W Gold",
+    price: 2800000,
+    tag: "Power Supply",
+    image: "/CorsairSupply.jpg"
+  },
+  {
+    id: 7,
+    name: "AMD Ryzen 9 7950X",
+    price: 8900000,
+    tag: "CPU",
+    image: "/Ryzen9.jpg"
+  },
+  {
+    id: 8,
+    name: "Gigabyte GeForce RTX 4070",
+    price: 10500000,
+    tag: "Graphic Card",
+    image: "/rtx4070.png"
+  },
+  {
+    id: 9,
+    name: "MATX Mini ATX",
+    price: 500000,
+    tag: "PC Case",
+    image: "/casingATX.jpeg"
+  },
+  {
+    id: 10,
+    name: "Intel Core i5-13400f",
+    price: 3500000,
+    tag: "CPU",
+    image: "/intelCorei5.png"
+  }
+];
 
   const filteredProducts = activeTab === 'All' 
     ? allProducts 
